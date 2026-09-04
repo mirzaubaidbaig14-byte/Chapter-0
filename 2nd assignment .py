3bad955bfe721c14f659abd660bf74ecbc8173b5
Ubaid Tahir <mirzaubaidbaig14@gmail.com>
2026-09-04 19:51:40 +0500

    2nd assignment .py

---
+++ 2nd assignment .py
@@ -0,0 +1,7 @@
+print("HELLO WORLD!")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("You are", age, "years old")
+print("Welcome to Python!")
