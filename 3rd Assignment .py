93a6931153258fa8098f932f50209041fc4b8ccc
Ubaid Tahir <mirzaubaidbaig14@gmail.com>
2026-09-04 20:15:41 +0500

    3rd Assignment .py

---
+++ 3rd Assignment .py
@@ -0,0 +1,6 @@
+print(("Ubaid Tahir"+"\n" )*10000)
+print(("HELLO WORLD!"+"\t")*60)
+print(("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z" +"\t")*5000)
+print(("1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ,11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 ,  20 ,21 , 22 , 23 , 24 ,25" + "\n" )*8000)
+print(("1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ,11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 ,  20 ,21 , 22 , 23 , 24 ,25" + "\t" )*8000)
+print(("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z" +"\n")*5000)
